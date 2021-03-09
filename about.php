f7d8151b8f052a519c8f32361dcf5ff8c0b2dbd9
Ebenezer <79905357+EbbyDanj@users.noreply.github.com>
2021-03-09 03:57:32 +0100

    New file
    this is to ease the stress of upload

---
+++ about.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>About | Alheri Royal Hotel and Apartment</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Marimar Hotel template project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/animate.css">
+<link href="plugins/jquery-datepicker/jquery-ui.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/about.css">
+<link rel="stylesheet" type="text/css" href="styles/about_responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+	
+	<?php include_once("header.php");?>
+
+	<!-- Home -->
+
+	<div class="home">
+		<div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/about.jpg" data-speed="0.8"></div>
+		<div class="home_container d-flex flex-column align-items-center justify-content-center">
+			<div class="home_title"><h1>About Us</h1></div>
+		</div>
+	</div>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+			<div class="row row-eq-height">
+
+				<!-- Intro Content -->
+				<div class="col-lg-6">
+					<div class="intro_content">
+						<div class="section_title">
+							<div>Hello</div>
+							<h1>Amazing Hotel in Gwarimpa Estate</h1>
+						</div>
+						<div class="intro_text">
+							<p>Alheri Royal Hotel & Apartment is a home away from home where we focus on developing a close relationship with our customers and matching that need for relaxation with the standard of our service so please make your reservation with us at Alheri Royal hotel & Apartment being a suitable option for value delivery and standard service where hospitality comes to us naturally.</p>
+						</div>
+						<div class="button intro_button"><a href="#">read more</a></div>
+					</div>
+				</div>
+
+				<!-- Intro Image -->
+				<div class="col-lg-6">
+					<div class="intro_image">
+						<div class="background_image" style="background-image:url(images/intro.jpg)"></div>
+						<img src="images/intro.jpg" alt="">
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Offering -->
+
+	<div class="offering">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title text-center">
+						<div>Resort</div>
+						<h1>What we offer</h1>
+					</div>
+				</div>
+			</div>
+			<div class="row offering_row">
+				
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image"><img src="images/offer_1.jpg" alt=""></div>
+						<div class="offer_content text-center">
+							<div class="offer_title"><h3>Lodging</h3></div>
+							<div class="offer_text">
+								<p>The rooms/apartment are clean and tastefully furnished, air conditioned with satellite television and free wireless internet</p>
+							</div>
+							<div class="offer_button"><a href="#">Book Now</a></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image"><img src="images/offer_2.jpg" alt=""></div>
+						<div class="offer_content text-center">
+							<div class="offer_title"><h3>Seminar Room</h3></div>
+							<div class="offer_text">
+								<p>The seminar room is well-equipped air conditioned with white writing board, flip chat board with paper and multimedia projector, it seats 15 persons.</p>
+							</div>
+							<div class="offer_button"><a href="#">Book Now</a></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image"><img src="images/offer_3.jpg" alt=""></div>
+						<div class="offer_content text-center">
+							<div class="offer_title"><h3>Restaurant</h3></div>
+							<div class="offer_text">
+								<p>Our restaurant is open 24 hours and 7 days of the week and offers a full range of menu of excellent National and Continental dishes</p>
+							</div>
+							<div class="offer_button"><a href="#">View Menu</a></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image"><img src="images/offer_4.jpg" alt=""></div>
+						<div class="offer_content text-center">
+							<div class="offer_title"><h3>Laundry</h3></div>
+							<div class="offer_text">
+								<p>Tidying up your worn cloths is never a problem as we have well trained personel that professionaly handle your Laundry.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image"><img src="images/offer_5.jpg" alt=""></div>
+						<div class="offer_content text-center">
+							<div class="offer_title"><h3>Bar</h3></div>
+							<div class="offer_text">
+								<p>Enjoy a comfortable atmostphere for relaxation as you order for your favourite drink.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image"><img src="images/offer_6.jpg" alt=""></div>
+						<div class="offer_content text-center">
+							<div class="offer_title"><h3>Gymn</h3></div>
+							<div class="offer_text">
+								<p>You can always stay fit with our gymn.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Discover -->
+
+	<div class="discover">
+
+		<!-- Discover Content -->
+		<div class="discover_content">
+			<div class="container">
+				<div class="row">
+
+					<!-- Discover Content -->
+					<div class="col-xl-5 col-lg-6">
+						<div class="section_title">
+							<div>Alheri</div>
+							<h1>Royal Hotel & Apartment</h1>
+						</div>
+						<div class="discover_highlight">
+							<p>Our room rates are competitive and our Guests experience the personal touch and friendliness from our Staff.</p>
+						</div>
+						<div class="discover_text">
+							<p>All our Rooms / Apartments are elegantly furnished with executive furniture include luxury en-suite facilities with complimentary amenities pack, flat screen LCD TV, tea/coffee making facilities, fan, free wifi and the finest pure white linen and towels.</p>
+						</div>
+						<div class="button discover_button"><a href="booking">Book Now</a></div>
+					</div>
+
+					<!-- Milestones -->
+					<div class="col-lg-6 offset-xl-1">
+						<div class="milestones d-flex flex-row align-items-start justify-content-start flex-wrap">
+							
+							<!-- Milestone -->
+							<div class="milestone">
+								<div class="milestone_counter" data-end-value="21">0</div>
+								<div class="milestone_text">Rooms</div>
+							</div>
+
+							<!-- Milestone -->
+							<div class="milestone">
+								<div class="milestone_counter" data-end-value="3">0</div>
+								<div class="milestone_text">Apartments</div>
+							</div>
+
+							<!-- Milestone -->
+							<div class="milestone">
+								<div class="milestone_counter" data-end-value="2">0</div>
+								<div class="milestone_text">Seminar Room / Hall</div>
+							</div>
+
+							<!-- Milestone -->
+							<div class="milestone">
+								<div class="milestone_counter" data-end-value="40" data-sign-after="k">0</div>
+								<div class="milestone_text">Happy Clients</div>
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- Footer -->
+
+	<?php include_once("footer.php");?>
+	
+</div>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="styles/bootstrap-4.1.2/popper.js"></script>
+<script src="styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/OwlCarousel2-2.3.4/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/progressbar/progressbar.min.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="plugins/jquery-datepicker/jquery-ui.js"></script>
+<script src="js/about.js"></script>
+</body>
+</html>
